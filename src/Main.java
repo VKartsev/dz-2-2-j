7ad9b9cabc2a63645df4796064689f3bbe5d8b7f
VKartsev <vach-partner@mail.ru>
2022-05-10 13:04:47 +0300

    Bonus 2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int client = 300;
+        short payment = 1100;
+        System.out.println("Сумма на счете: " + (client + payment));
+        if (payment > 1000) {
+            System.out.println("Бонус:" + (payment + client) / 100);
+        }
+
+    }
+}
+
